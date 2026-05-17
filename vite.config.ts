3c5e35cb8d02c8c5441782e2b0a5d66cd0a7d8c7
reebu mukherjee <reebu.mukherjee@oracle.com>
2026-05-17 16:19:17 +0530

    fix paths and frontend now loading.

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/interceptor/",
+  base: "/travian-tools/",
   plugins: [react()],
   server: {
     port: 5173,
